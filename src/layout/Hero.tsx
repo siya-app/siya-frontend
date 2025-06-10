c6ebfd8b74761aed91bad8ce6a3f30580fbe4f44
AuroraHumo <carles.casallachs@gmail.com>
2025-06-10 10:54:12 +0200

    corrected colors and footer navigation

--- src/layout/Hero.tsx
+++ src/layout/Hero.tsx
@@ -4,8 +4,12 @@ const Hero = () => {
     return (
         <div className="flex bg-gray-100">
             <div className="mx-auto">
-                <img src="src/assets/siya-title-bg-transp.svg" className="mx-auto w-30 h-30" />
-                <p className="text-xl text-siya-red">La teva nova app de terraceo.</p>
+                <img src="src/assets/siya-title-bg-transp.svg"
+                className="mx-auto
+                w-30
+                h-30" />
+                <p className="text-xl
+                text-siya-red">La teva nova app de terraceo.</p>
             </div>
             <div className="mx-auto">
                 <FetchWeather />
