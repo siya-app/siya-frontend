7b1363b0043bab6a3cfa52c6d8e9e441655f3a46
AuroraHumo <carles.casallachs@gmail.com>
2025-06-16 16:22:59 +0200

    updated route terrasssa in navbar

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
             <nav className="flex flex-wrap justify-center sm:justify-start gap-4 text-sm sm:text-base">
                 <ul className="flex space-x-4">
                     <li><Link to="/" className="hover:underline">A prop teu</Link></li>
-                    <li><Link to="/buscar-terraza" className="hover:underline">Buscar Terrassa</Link></li>
+                    <li><Link to="/buscar-terrassa" className="hover:underline">Buscar Terrassa</Link></li>
                     <li><Link to="/perfil" className="hover:underline">Perfil</Link></li>
                 </ul>
             </nav>
