69fa3fe693105a93f6c669ece71b5fabb2d4fa37
OhMyLaia <ohmylaia@live.com>
2025-06-17 22:28:59 +0200

    blobs can now be selected on toggle

--- src/components/BlobList.tsx
+++ src/components/BlobList.tsx
@@ -3,6 +3,7 @@ import { getBlobs } from "../services/blobList.service";
 import { BLOB_TRANSLATIONS } from "../services/blobList.service";
 import type { CustomTerraceType } from "../types/zod/customTerrace-schema";
 import { useState } from "react";
+import SliderButton from "./slider/SliderButton";
 
 type BlobType = 'cover' | 'dietary' | 'emotional' | 'food' | 'placement';
 
@@ -20,34 +21,47 @@ export function BlobCarousel({
     // filteredTerraces
 }: BlobCarouselProps) {
 
-    const [isSelected, setIsSelected] = useState(false);
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
 
-    const toggleSelection = () => {
-        
-    }
+
+    const toggleSelection = (id: string) => {
+
+        setSelectedTags((prev) => {
+            const updated = prev.includes(id)
+                ? prev.filter(tag => tag !== id)
+                : [...prev, id];
+    
+            console.log("updated selectedTags:", updated);
+            return updated;
+        });
+
+    };
+
 
     const blobs: Record<BlobType, Record<string, string>> = getBlobs();
     const translations = BLOB_TRANSLATIONS[type];
     const categoryTitle = BLOB_TRANSLATIONS.categories[type];
 
     return (
-        <div className="shadow-lg shadow-siya-terciario mb-5 pb-2 flex flex-col justify-start">
-            <h2 className="text-lg text-siya-dark-green font-semibold ms-3 mb-3 system-sans">{categoryTitle}</h2>
+        <div className="shadow-lg shadow-siya-terciario mb-5 pb-2 mt-3 flex flex-col justify-start">
+            <h2 className="text-lg text-siya-dark-green font-semibold ms-3 mb-2 mt-2 system-sans">{categoryTitle}</h2>
             <ScrollSnap>
                 {Object.entries(blobs[type]).map(([id, src]) => (
+
                     <div key={id} className="snap-start shrink-0 w-[5em] mx-1">
-                        <div className={"flex flex-col justify-start items-center p-2" }>
+                        <div className={"flex flex-col justify-start items-center p-2"}>
                             <img
                                 src={src}
                                 alt={translations[id as keyof typeof translations]}
                                 className="w-20 h-20 object-contain"
                             />
-                            <button className="text-xs text-center
-                        capitalize siya2-bg rounded-xl siya3-text
-                        p-1.5 shadow-md shadow-gray-600
-                        "> {/* Added label */}
-                                {translations[id as keyof typeof translations]}
-                            </button>
+                            <SliderButton
+                                key={`button-${id}`}
+                                onClick={() => toggleSelection(id)}
+                                tagName={translations[id as keyof typeof translations]}
+                                id={id}
+                                selectedTags={selectedTags}
+                            />
                         </div>
                     </div>
                 ))}
