b9d4b57cb540b3d45f93cf50880fccbc0a083646
OhMyLaia <ohmylaia@live.com>
2025-06-18 11:33:16 +0200

    created tags for restaurants

--- src/pages/FilterPage.tsx
+++ src/pages/FilterPage.tsx
@@ -7,31 +7,23 @@ import TerraceSlider from "../components/slider/TerraceSlider";
 import { useTerraceList } from "../hooks/useTerraceList";
 import type { CustomTerraceType } from "../types/zod/customTerrace-schema";
 
+//restaurants with inserted tags:
+// china garden - url: 0032e31f-f65f-459d-bfad-ab5b38fd3164/tags
+// can lampazas - url: 003e3afe-ebe0-400f-9c93-9cadec1633d2/tags
+// champanillo - url: 12c24ece-698e-42f3-b344-622a008eb221/tags
+// divina stefy - url: 263bd00c-b105-4ca2-8230-27372a5df6a4/tags
+// el tomas de sarria - url: 680fbbea-ccb3-4b3d-980e-406d1fca06b5/tags
+// xix kebab - url: 8f87f1e9-85fc-4b85-b408-25dbbe92aac2/tags
+// la terraza miro - url: a93749f0-7efe-4266-ab43-9e80234cb701/tags
+// nori sushi&cocktails - url: abc19588-83cd-4053-8d13-5f75de5e54f3/tags
+
+
 const FilterPage = () => {
 
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const { terraceList } = useTerraceList();
   const [filteredTerraces, setFilteredTerraces] = useState<CustomTerraceType[]>([]);
 
-
-  // const handleSearch = (query: string) => {
-  //   console.log("Searching for:", query);
-  //   // API call or filtering logics in here
-
-  //   // const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
-  //   // const [terraces, setTerraces] = useState<Terrace[]>([]); // From API or context
-
-  //   // // Filter terraces based on selected filters
-  //   // const filteredTerraces = filterTerraces(terraces, selectedFilters);
-
-  //   // const handleFilterToggle = (filter: string) => {
-  //   //   setSelectedFilters(prev =>
-  //   //     prev.includes(filter)
-  //   //       ? prev.filter(f => f !== filter)
-  //   //       : [...prev, filter]
-  //   //   );
-  //   // };
-  // };
 
   const toggleSelection = (id: string) => {
 
