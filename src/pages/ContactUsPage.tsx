94cbfddabcd7232fab56e6329cd786f12053b0f5
OhMyLaia <ohmylaia@live.com>
2025-06-21 20:28:16 +0200

    created contactUs page

---
+++ src/pages/ContactUsPage.tsx
@@ -0,0 +1,42 @@
+import { PiInstagramLogoBold } from "react-icons/pi";
+import { AiFillMail } from "react-icons/ai";
+import calling from '../assets/calling.png';
+import agreement from '../assets/agreement.png'
+
+
+
+function contactUsPage() {
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <div className="
+            bg-gray-100
+            place-items-center
+            text-wrap
+            mt-6 text-gray-700
+            rounded-xl
+            border-siya-principal
+            border-r-4 border-b-4
+            w-fit p-4">
+                <p>
+                    <AiFillMail className="inline-icon me-2"/>
+                    siya.bcn.app@gmail.com</p>
+            </div>
+            <div className="
+            bg-gray-100
+            place-items-center
+            text-wrap
+            mt-6 text-gray-700
+            rounded-xl
+            border-siya-principal
+            border-r-4 border-b-4
+            w-fit p-4">
+                <p>
+                <PiInstagramLogoBold className="inline-icon me-2"/>
+                    @siya_bcn</p>
+            </div>
+            <img className="animate-bounce w-1/2 mt-15" src={agreement}></img>
+        </div>
+    )
+}
+
+export default contactUsPage
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,10 +6,11 @@ import FilterPage from "../pages/FilterPage";
 import BookingPage from "../pages/BookingPage";
 import NotFoundPage from "../pages/NotFoundPage";
 import TerraceDetailsView from "../features/terraces/TerraceDetailsView";
-import AboutUs from "../pages/AboutUs";
+import AboutUs from "../pages/AboutUsPage";
 import LogIn from '../pages/LogIn'
 import SignUp from '../pages/SignUp'
 import PartnerPage from "../pages/PartnerPage";
+import ContactUsPage from "../pages/contactUsPage";
 
 const AppRoutes = () => {
   return (
@@ -22,6 +23,7 @@ const AppRoutes = () => {
       <Route path="/login" element={<LogIn/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
       <Route path="/partners" element={<PartnerPage/>}/>
+      <Route path="/contacte" element={<ContactUsPage/>}/>
       <Route
         path="/terrassa/:id"
         element={<TerraceDetailsView />}
