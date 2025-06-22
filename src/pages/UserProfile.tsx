acdd2b0a60d380751a5aa355e3dd4d4f5cfd53b6
Mika7596 <mika7596@gmail.com>
2025-06-22 08:19:07 +0200

    Converted Delete Account component in a modal one.

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,45 +1,40 @@
-import { useEffect, useState } from 'react';
-import React from 'react';
-import API from '../services/apiUser';
-import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
-import TerraceClaim from '../features/terrace-claim/TerraceClaim';
-import TerraceSlider from '../components/slider/TerraceSlider';
-import { useTerraceList } from '../hooks/useTerraceList';
-import Button from '../components/Button';
+import { useEffect, useState } from "react";
+import React from "react";
+import API from "../services/apiUser";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import TerraceClaim from "../features/terrace-claim/TerraceClaim";
+import TerraceSlider from "../components/slider/TerraceSlider";
+import { useTerraceList } from "../hooks/useTerraceList";
+import Button from "../components/Button";
+import DeleteAccount from "../features/delete-account/DeleteAccount";
 
 export default function Profile() {
   const [user, setUser] = useState<any>(null);
   const navigate = useNavigate();
   const { terraceList } = useTerraceList();
   const [openSection, setOpenSection] = useState<boolean>(false);
-
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-
-    const storedUser = localStorage.getItem('user');
-    const storedToken = localStorage.getItem('token')
+    const storedUser = localStorage.getItem("user");
+    const storedToken = localStorage.getItem("token");
     console.log(storedUser);
 
     if (storedUser) {
-
       setUser(JSON.parse(storedUser));
-
     } else {
-
-      navigate('/login');
+      navigate("/login");
     }
   }, []);
 
-
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    navigate('/');
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    navigate("/");
   };
 
   if (!user) return <p>Carregant dades de l'usuari...</p>;
-
 
   return (
     <>
@@ -54,8 +49,8 @@ export default function Profile() {
         </div>
       </div>
       <Button
-      onClick={() => navigate("/buscar-terrassa")}
-      className={`text-primary-content px-4 py-2 mt-8
+        onClick={() => navigate("/buscar-terrassa")}
+        className={`text-primary-content px-4 py-2 mt-8
         m-4 bg-gr bg-siya-principal text-white rounded-full
         flex justify-between items-center
         toggle-height
@@ -63,12 +58,9 @@ export default function Profile() {
       >
         Reservar taula
       </Button>
-      <TerraceSlider
-        list={terraceList}
-        orderBy={"nearby"}
-      />
+      <TerraceSlider list={terraceList} orderBy={"nearby"} />
       <div
-        onClick={() => setOpenSection(prev => !prev)}
+        onClick={() => setOpenSection((prev) => !prev)}
         className="cursor-pointer collapse-title
         text-primary-content px-4 py-2
         m-4 bg-siya-terciario
@@ -77,7 +69,9 @@ export default function Profile() {
         rounded
         "
       >
-        <h3 id="claimTerrace" className="text-xl m-2">Ets propietari d'una terrassa?</h3>
+        <h3 id="claimTerrace" className="text-xl m-2">
+          Ets propietari d'una terrassa?
+        </h3>
         {openSection ? (
           <FaChevronUp className="siyaDark-text" />
         ) : (
@@ -85,19 +79,31 @@ export default function Profile() {
         )}
       </div>
       <div
-  className={`toggle-height transition-all duration-600 ${openSection ? 'max-h-[1000px]' : 'max-h-0'}`}
->
-  <div className="p-4">
-    <TerraceClaim />
-  </div>
-</div>
-<div className='flex justify-center m-5'>
-  <Button
-  onClick={handleLogout}
-  className={`border-2 border-siya-principal rounded-xl p-2 text-siya-principal w-fit`}
-  >Log out</Button>
-</div>
+        className={`toggle-height transition-all duration-600 ${
+          openSection ? "max-h-[1000px]" : "max-h-0"
+        }`}
+      >
+        <div className="p-4">
+          <TerraceClaim />
+        </div>
+      </div>
+      <div className="m-auto w-fit">
+        <Button
+          onClick={() => setShowModal(true)}
+          className="border-2 border-siya-principal rounded-xl p-2 bg-gr bg-siya-principal text-white"
+        >
+          Eliminar compte
+        </Button>
+      </div>
+      <DeleteAccount isOpen={showModal} onClose={() => setShowModal(false)} />
+      <div className="flex justify-center m-5">
+        <Button
+          onClick={handleLogout}
+          className={`border-2 border-siya-principal rounded-xl p-2 text-siya-principal w-fit`}
+        >
+          Log out
+        </Button>
+      </div>
     </>
-  )
+  );
 }
-
