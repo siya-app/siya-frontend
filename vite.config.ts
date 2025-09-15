cbfe5dadedae55af39b68408901b208280d3da66
rorogab <andradeojedagabriela@gmail.com>
2025-09-15 18:57:17 +0200

    Change in vite.config.ts so it can listen to docker as well

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5173, // Dev server port (optional, default is 5173)
-    host: true, // Listen on 0.0.0.0, required for Docker
-    strictPort: true, // Fail if port is already in use
+    port: 5173,
+    host: true, // listen on 0.0.0.0 for Docker
+    strictPort: true, // fail if port already in use
   },
 });
