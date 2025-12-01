9c9075aaf41f5860cd21ccde34698cdbb5420e33
OhMyLaia <ohmylaia@live.com>
2025-12-01 16:31:58 +0100

    created spinner component to reuse

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+import { FaSpinner } from 'react-icons/fa';
+
+function Spinner() {
+    return (
+        <FaSpinner className="animate-spin text-center text-siya-principal text-4xl mx-auto" />
+    )
+}
+
+export default Spinner
